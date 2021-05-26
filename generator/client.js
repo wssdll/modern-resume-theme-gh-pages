ad4fd4e57204b0124719aab347352e6808eae5ff
royal029 <wssdll15@gmail.com>
2021-05-26 16:34:46 +0800

    update: client & cv

--- generator/client.js
+++ generator/client.js
@@ -2,17 +2,23 @@ const fs = require('fs')
 const cheerio = require('cheerio');
 const CryptoJS = require('crypto-js');
 
-function readFile(path){//异步执行
+function readFile(path){
     fs.readFile(path,function(err,data){
         if(err){
             console.log(err);
         }else{
             const $ = cheerio.load(data);
 
+            // remove unused script
             $('body script').remove()
+
+            // image path fix, depending on the deployment environment
+            let image =  $('.profile-img')[0]
+            image.attribs.style = image.attribs.style.replace(/\//,'./')
+
             let file = $('body').html()
 
-            let ciphertext = CryptoJS.AES.encrypt(file, '')
+            let ciphertext = CryptoJS.AES.encrypt(file, 'password')
             fs.writeFile('template/resume', ciphertext, err => {
                 if (err) throw err
                 console.log('文件已被写入')
